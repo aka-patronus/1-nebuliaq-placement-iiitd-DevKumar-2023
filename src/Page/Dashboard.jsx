f7e3de5825adb69118ca28f77be4bc6331f75c80
aka-patronus <2001devk@gmail.com>
2024-08-13 11:18:49 +0530

    added dummy data and initied app

---
+++ src/Page/Dashboard.jsx
@@ -0,0 +1,6 @@
+import React, { useState, useMemo } from 'react';
+import { data } from '../data';
+import {
+  Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, TextField, Select, MenuItem, FormControl, InputLabel, Typography, Box
+} from '@mui/material';
+
